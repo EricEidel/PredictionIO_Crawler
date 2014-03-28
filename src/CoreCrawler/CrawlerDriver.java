e3d6bb1ce27e5e5946c72cca939312359cfa28d1
Eric_eidl <ninuson123@gmail.com>
2014-03-28 09:51:28 -0600

    Added final touches to connect to the engine and submit the items. Commented out the sub-reddits, a 1000 links should do for the demo.

--- src/CoreCrawler/CrawlerDriver.java
+++ src/CoreCrawler/CrawlerDriver.java
@@ -3,8 +3,18 @@ package CoreCrawler;
 import java.util.List;
 
 import json.mapping.RedditLink;
-import json.mapping.RedditSubreddit;
+import wrapper.PredIOConn;
 import exception.RedditException;
+
+/**
+ * @author: Eric Eidelberg
+ * @email: ninuson123@gmail.com
+ * 
+ * This is part of the demo application for the Prediction-IO recommendation engine.
+ * This program establishes connection and gets the first 1,000 items on the front page of reddit. 
+ * It then sends those to the recommendation engine for later retrieval by the client application.
+ * 
+ */
 
 public class CrawlerDriver 
 {	
@@ -22,8 +32,8 @@ public class CrawlerDriver
 			//    	    System.out.println(account.toString());
 
 			// Get top 25 subreddits.
-			List<RedditSubreddit> links_sub = reddit.subreddits("");
-			System.out.println("Number of subreddits exploring: " + links_sub.size());    
+			//			List<RedditSubreddit> links_sub = reddit.subreddits("");
+			//			System.out.println("Number of subreddits exploring: " + links_sub.size());    
 
 			System.out.println("Starting with main menu: ");
 			List<RedditLink> links = reddit.listingForMainPage();
@@ -43,51 +53,73 @@ public class CrawlerDriver
 				so_far = links_next.size();
 				System.out.println(links.size());
 			}
-
-			System.out.println("Done main page. Starting sub-reddits...");
-
-			Thread.sleep(10000); // Sleep for 10 seconds
 			
-			for (RedditSubreddit sub : links_sub)
+			// This part can feed another 21k~ links or so. Reddit allows only 30 requests per minute, hence the sleep timers.
+
+			System.out.println("Done main page. Added " + links.size() + " to the list.");
+//
+//			Thread.sleep(10000); // Sleep for 10 seconds
+//			
+//			for (RedditSubreddit sub : links_sub)
+//			{
+//				System.out.println("Starting with subreddit: " + sub.getUrl());
+//				String subreddit = sub.getUrl().replace("/r/", "").replace("/","");
+//
+//				List<RedditLink> links_for_sub =  reddit.listingFor(subreddit, "new");
+//								
+//				so_far = -1;
+//				while (so_far != 0)
+//				{
+//					RedditLink last_link = links_for_sub.get(links_for_sub.size()-1);
+//
+//					//Get an X amount of items from item Y
+//					List<RedditLink> links_next = reddit.listingFor(subreddit, 100, "t3_"+last_link.getId());
+//					System.out.println("Links delivered: " + links_next.size());
+//
+//					links_for_sub.addAll(links_next);
+//
+//					so_far = links_next.size();
+//					System.out.println(links_for_sub.size());
+//				}
+//				
+//				links.addAll(links_for_sub);
+//				links_for_sub.clear();
+//				
+//				Thread.sleep(10000); // Sleep for 10 sec
+//				System.out.println("Total links so far: " + links.size());
+//			}
+			
+			// ========================== BEGINING OF ENGINE CONNECTION ===========================================
+			
+			System.out.println("Creating connection to Recomendation Engine...");
+			/* set appurl to your API server */
+	        String appurl = "http://localhost:8000";
+	        /* set appkey for the engine. */
+	        String appkey_2 = "ZL70ye3wxYGlfJGVX077r8R4XsB6oM60PXoqDfxbgDk3pOppnxKCj02ztOAVLrET";
+			
+	        // Initiate the connector
+			PredIOConn conn = new PredIOConn(appurl, appkey_2);
+			// Set debug console printouts on.
+			conn.setDebug(true);
+			
+			System.out.println("Connected!");
+			System.out.println("Begin sending info to recommendation engine.");
+			
+			String[] itypes = {"main_page"};
+			
+			for (RedditLink link : links)
 			{
-				System.out.println("Starting with subreddit: " + sub.getUrl());
-				String subreddit = sub.getUrl().replace("/r/", "").replace("/","");
-
-				List<RedditLink> links_for_sub =  reddit.listingFor(subreddit, "new");
-								
-				so_far = -1;
-				while (so_far != 0)
-				{
-					RedditLink last_link = links_for_sub.get(links_for_sub.size()-1);
-
-					//Get an X amount of items from item Y
-					List<RedditLink> links_next = reddit.listingFor(subreddit, 100, "t3_"+last_link.getId());
-					System.out.println("Links delivered: " + links_next.size());
-
-					links_for_sub.addAll(links_next);
-
-					so_far = links_next.size();
-					System.out.println(links_for_sub.size());
-				}
-				
-				links.addAll(links_for_sub);
-				links_for_sub.clear();
-				
-				Thread.sleep(10000); // Sleep for 10 sec
-				System.out.println("Total links so far: " + links.size());
+				// t3_ is a static prefix for links on the reddit api
+				String link_id = "t3_"+link.getId();
+				conn.addItem(link_id, itypes);
 			}
 			
-			
+			System.out.println("Done!");
 		} 
 		catch( RedditException re ) 
 		{
 			//RedditException merely inherits Exception.
 			re.printStackTrace();
-		} 
-		catch (InterruptedException e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} 		
 	}
 }
